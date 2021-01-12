3686f9c1cade252a3ca3f25785c58ea04a33aa3f
Vladimir Agapov <kyleridrus@gmail.com>
2021-01-12 19:03:08 +0300

    add manifest

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>LaravelCMS</title>
+    <link rel="manifest" href="/manifest.json"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <style>
